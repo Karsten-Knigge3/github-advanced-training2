39fc57c658c76aa3d2b9ba41b7b920f60b5a01c0
Karsten-Knigge3 <knigge.karsten@gmx.de>
2026-01-22 13:22:01 +0100

    Create calculator.js
    Add calculator functions

---
+++ calculator.js
@@ -0,0 +1,12 @@
+// Simple Calculator
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+// TODO: Add multiply and divide functions
+
+module.exports = { add, subtract };
