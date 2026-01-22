9f30c12a4fc9e11f2f9f0e35dc2af72b26620167
Karsten-Knigge3 <knigge.karsten@gmx.de>
2026-01-22 20:59:19 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,5 +1,10 @@
 // Simple Calculator
 function add(a, b) {
+if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Both arguments must be numbers');
+    }
+    return a + b;
+}
     return a + b;
 }
 
