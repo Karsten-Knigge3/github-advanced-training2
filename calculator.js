5721a2aa220e3e9250c4f9d7ba7ac73e77c1dc8a
Karsten-Knigge3 <knigge.karsten@gmx.de>
2026-01-22 21:02:56 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -12,7 +12,22 @@ function subtract(a, b) {
 }
 
 // TODO: Add multiply and divide functions
+function multiply(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Both arguments must be numbers');
+    }
+    return a * b;
+}
 
+function divide(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Both arguments must be numbers');
+    }
+    if (b === 0) {
+        throw new Error('Cannot divide by zero');
+    }
+    return a / b;
+}
 module.exports = { add, subtract };
 
 // Neue Funktion erforderlich
